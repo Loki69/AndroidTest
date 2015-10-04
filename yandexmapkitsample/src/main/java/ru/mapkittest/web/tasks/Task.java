8c1d65239485861be8bfe4e28f41a5035736c9ea
Pavel Garanin <mur0@mail.ru>
2015-10-04 17:41:53 +0300

    clear and Modified task to data structure

--- yandexmapkitsample/src/main/java/ru/mapkittest/web/tasks/Task.java
+++ yandexmapkitsample/src/main/java/ru/mapkittest/web/tasks/Task.java
@@ -1,132 +1,84 @@
 package ru.mapkittest.web.tasks;
 
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import ru.yandex.yandexmapkit.utils.GeoPoint;
 
 /**
  * Created by Павел on 04.10.2015.
  */
 public class Task {
-    private Location location;
-    private List<Price> prices = new ArrayList<Price>();
-    private String ID;
-    private String locationText;
-    private Integer price;
-    private String title;
-    private String longText;
-    private Boolean translation;
-    private String text;
-    private Integer zoomLevel;
-    private Integer date;
-    private Integer endDate;
+    public final GeoPoint location;
+    public  List<Price> prices = new ArrayList<Price>();
+    public final String ID;
+    public final String locationText;
+    public final int price;
+    public final String title;
+    public final String longText;
+    public final boolean translation;
+    public final String text;
+    public final int zoomLevel;
+    public final Date date;
+    public final Date endDate;
 
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setLocation(Location location) {
+    private Task(GeoPoint location, String ID, String locationText, int price, String title, String longText, Boolean translation, String text, int zoomLevel, Date date, Date endDate) {
         this.location = location;
-    }
-
-    public List<Price> getPrices() {
-        return prices;
-    }
-
-    public void setPrices(List<Price> prices) {
-        this.prices = prices;
-    }
-
-    public String getLocationText() {
-        return locationText;
-    }
-
-    public void setLocationText(String locationText) {
-        this.locationText = locationText;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLongText() {
-        return longText;
-    }
-
-    public void setLongText(String longText) {
-        this.longText = longText;
-    }
-
-    public Boolean getTranslation() {
-        return translation;
-    }
-
-    public void setTranslation(Boolean translation) {
-        this.translation = translation;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Integer getZoomLevel() {
-        return zoomLevel;
-    }
-
-    public void setZoomLevel(Integer zoomLevel) {
-        this.zoomLevel = zoomLevel;
-    }
-
-    public Integer getDate() {
-        return date;
-    }
-
-    public void setDate(Integer date) {
-        this.date = date;
-    }
-
-    public String getID() {
-        return ID;
-    }
-
-    public void setID(String ID) {
         this.ID = ID;
-    }
-
-    public Integer getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Integer endDate) {
+        this.locationText = locationText;
+        this.price = price;
+        this.title = title;
+        this.longText = longText;
+        this.translation = translation;
+        this.text = text;
+        this.zoomLevel = zoomLevel;
+        this.date = date;
         this.endDate = endDate;
     }
 
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
+    private Task() {
+        this.location = new GeoPoint(0, 0);
+        this.ID = "";
+        this.locationText = "";
+        this.price = 0;
+        this.title = "";
+        this.longText = "";
+        this.translation = false;
+        this.text = "";
+        this.zoomLevel = 0;
+        this.date = new Date();
+        this.endDate = new Date();
     }
 
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
+    public static Task getInit(JSONObject task) {
+        Task result = null;
+        try {
+            GeoPoint location = (task.has("lon") && task.has("lat")) ? (new GeoPoint(task.getDouble("lon"), task.getDouble("lat"))) : (new GeoPoint(0, 0));
+            String ID = (task.has("ID")) ? (task.getString("ID")) : ("");
+            String locationText = (task.has("locationText")) ? (task.getString("locationText")) : ("");
+            int price = (task.has("price")) ? (task.getInt("price")) : (0);
+            String title = (task.has("title")) ? (task.getString("title")) : ("");
+            String longText = (task.has("longText")) ? (task.getString("longText")) : ("");
+            boolean translation = (task.has("translation")) ? (task.getBoolean("translation")) : (false);
+            String text = (task.has("text")) ? (task.getString("text")) : ("");
+            int zoomLevel = (task.has("zoomLevel")) ? (task.getInt("zoomLevel")) : (9);
+            Date date = (task.has("date")) ? (new Date(task.getString("date"))) : (new Date());
+            Date endDate = (task.has("endDate")) ? (new Date(task.getString("endDate"))) : (new Date());
+            result = new Task(location,ID,locationText,price,title,longText,translation,text,zoomLevel,date,endDate);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            result = new Task();
+        }
+        return result;
     }
-
+    public boolean isEmpty(){
+        return new GeoPoint(0,0).equals(this.location) && this.ID.isEmpty();
+    }
 }
--- yandexmapkitsample/src/main/java/ru/mapkittest/web/tasks/Tasks.java
+++ yandexmapkitsample/src/main/java/ru/mapkittest/web/tasks/Tasks.java
@@ -1,24 +1,47 @@
 package ru.mapkittest.web.tasks;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import ru.mapkittest.web.connec.WebConnector;
+
 /**
  * Created by Павел on 04.10.2015.
  */
 public class Tasks {
-    private static  Tasks singleton;
+    private static Tasks singleton;
     private final String urlData = "http://test.boloid.com:9000/tasks";
     private List<Task> tasks = new ArrayList<Task>();
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
     private Tasks() {
+
+    }
+
+    private void init() throws MalformedURLException, JSONException {
+        WebConnector wc = new WebConnector(this.urlData);
+        parsingToTasks(wc.getDataFromPage());
+    }
+
+    private void parsingToTasks(String data) throws JSONException {
+        JSONObject jsonRootObject = new JSONObject(data);
+        JSONArray jsonArray = jsonRootObject.optJSONArray("tasks");
+        for (int i = 0; i < jsonArray.length(); i++) {
+            tasks.add();
+            Log.e("Json", jsonObject.toString());
+        }
     }
 
     public static Tasks getInit() {
-        if(singleton == null){
+        if (singleton == null) {
             singleton = new Tasks();
         }
         return singleton;
